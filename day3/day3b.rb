ef7348a8bd318aa3bd3d57597913cb34ecaab994
Patrick Hurley <phurley@invoca.com>
2024-12-03 09:52:31 -0500

    More functional

--- day3/day3b.rb
+++ day3/day3b.rb
@@ -1,23 +1,18 @@
 # frozen_string_literal: true
 
-input = ARGF
-        .readlines
-        .join('')
-        .scan(/mul\(\d+,\d+\)|don't\(\)|do\(\)/)
-
-nums = input.reduce([true, []]) do |(state, acc), x|
-    case x
+pp ARGF
+  .readlines
+  .join('')
+  .scan(/mul\(\d+,\d+\)|don't\(\)|do\(\)/)
+  .reduce([true, []]) { |(state, acc), op| 
+    case op
     when 'do()'
       [true, acc]
     when "don't()"
       [false, acc]
     when /mul\((\d+),(\d+)\)/
-      if state
-        [state, acc << $1.to_i * $2.to_i]
-      else
-        [state, acc]
-      end
+      state ? [state, acc + [$1.to_i * $2.to_i]] : [state, acc]
     end
-end
-
-pp nums.last.sum
+  }
+  .last
+  .sum
